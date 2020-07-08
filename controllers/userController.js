86f93179ade30f50ea2a7a4290320ebfacf47d98
Oscar Monroy <oscarmonroy657@gmail.com>
2020-07-07 18:57:05 -0600

    Feature create user is ready and create web token is also ready

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,44 @@
 const User = require('../models/User')
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
+  // Validacion de express-validator
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    res.status(400).json({errors: errors.array()})
+  }
+
+  const { userEmail, userPassword } = req.body;
+
   try {
-    let user;
+    let user = await User.findOne({userEmail});
+
+    if(user){
+      return res.status(400).json({ msg: 'El usuario ya existe'});
+    }
 
     user = new User(req.body)
+    // Hashear password
+    const salt = await bcryptjs.genSalt(10);
+    user.userPassword = await bcryptjs.hash(userPassword, salt);
+
     await user.save();
 
-    res.send('Usuario creado correctamente');
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(payload, process.env.SECRET_WORD, {
+      expiresIn: 3600
+    }, (error, token) => {
+      if(error) throw error;
+
+      res.json({ token });
+    })
     
   } catch (error) {
     console.log(error);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const userControler = require('../controllers/userController');
+const { check } = require('express-validator');
 
 const router = express.Router();
 
-router.post('/', userControler.createUser);
+router.post('/', 
+  [
+    check('userName', 'El nombre es obligatorio').not().isEmpty(),
+    check('userEmail', 'Ingresa un email válido').isEmail(),
+    check('userPassword', 'La contraseña debe de tener al menos seis caracteres').isLength({ min: 6 })
+  ]
+,userControler.createUser);
 
 module.exports = router;
